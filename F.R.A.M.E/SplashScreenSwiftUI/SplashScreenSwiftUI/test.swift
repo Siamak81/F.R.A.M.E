7138d7da26db0bf951e6f108e6cc934b9530ee0f
Siamak Zeinali <86515911+Siamak81@users.noreply.github.com>
2024-07-17 22:57:23 +0330

    Update Files

---
+++ F.R.A.M.E/SplashScreenSwiftUI/SplashScreenSwiftUI/test.swift
@@ -0,0 +1,20 @@
+//
+//  test.swift
+//  F.R.A.M.E
+//
+//  Created by siamak on 4/16/1402 AP.
+//
+
+import SwiftUI
+
+struct test: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct test_Previews: PreviewProvider {
+    static var previews: some View {
+        test()
+    }
+}
